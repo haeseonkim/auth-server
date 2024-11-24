937e78e06bf72202853ddc818ae4ad1628be7e59
haeseonkim <khs51777@naver.com>
2024-11-24 14:55:07 +0900

    feat: DB 연결 정보 추가, 엔티티 클래스 설정

---
+++ src/main/java/com/abab/auth/entity/User.java
@@ -0,0 +1,31 @@
+package com.abab.auth.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Entity
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "app_user")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    private String username;
+
+    @Column(nullable = false)
+    private long createdAt;
+}
